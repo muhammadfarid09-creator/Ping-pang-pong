a2fdb479b0ffccad6b6642417cd3924d51502847
muhammadfarid09-creator <muhammad.farid_09@idn.sch.id>
2026-01-10 11:47:51 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 from pygame import *
 
 background_color = (238, 255, 208)
@@ -17,24 +18,48 @@ class GameSprite(sprite.Sprite):
         self.rect.y = player_y
 
     def reset(self):
-        window.blit(self.image, (self.rect.x, self.rect.y))
+        window.blit(self.image, (self.rect.x, self.rect.y)) 
 
-raket_1 =  GameSprite('raket.png', 30, 200, 5, 40, 115)
-raket_2 = GameSprite('raket.png', 520, 200, 5, 40, 115)
+class Player(GameSprite):
+    def move_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys [K_s] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+
+    def move_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+
+
+raket_1 =  Player('raket.png', 30, 200, 5, 40, 115)
+raket_2 = Player('raket.png', 520, 200, 5, 40, 115)
 ball = GameSprite('tennis_ball.png', 200, 200, 5, 40, 40)
 
 
 game = True
+clock = time.Clock()
+FPS = 60
 
 while  game:
+    window.fill(background_color)
+    raket_1.move_l()
+    raket_2.move_r()
+
+
     for e in event.get ():
         if e.type == QUIT:
             game = False
     
     raket_1.reset()
-    raket_2.reset()
+    raket_2.reset() 
     ball.reset()
 
 
     display.update()
+    clock.tick(FPS)
 
