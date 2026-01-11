e0c5b7d2324261ed25a199e6891d06b6e75bd633
muhammadfarid09-creator <muhammad.farid_09@idn.sch.id>
2026-01-11 10:58:00 +0700

    finish main py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 from pygame import *
 
 background_color = (238, 255, 208)
@@ -40,26 +39,63 @@ raket_1 =  Player('raket.png', 30, 200, 5, 40, 115)
 raket_2 = Player('raket.png', 520, 200, 5, 40, 115)
 ball = GameSprite('tennis_ball.png', 200, 200, 5, 40, 40)
 
+speed_x = 3
+speed_y = 3
+
+font.init()
+font = font.Font(None, 35)
+lose1 = font.render('PEMAIN SATU KALAH', True, (200, 0, 0))
+lose2 = font.render('PEMAIN DUA KALAH', True, (200, 0, 0))
+
+
+
 
 game = True
 clock = time.Clock()
 FPS = 60
 
-while  game:
-    window.fill(background_color)
-    raket_1.move_l()
-    raket_2.move_r()
+finish = False
 
-
+while  game:  
     for e in event.get ():
         if e.type == QUIT:
             game = False
-    
-    raket_1.reset()
-    raket_2.reset() 
-    ball.reset()
+
+    if finish != True :
+
+        window.fill(background_color)
+        raket_1.move_l()
+        raket_2.move_r()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+
+        if ball.rect.y > win_height - 50 or ball.rect.y < 0:
+            speed_y *= -1
+
+        if sprite.collide_rect(raket_1, ball) or sprite.collide_rect(raket_2, ball):
+            speed_x *= -1
+            speed_y *= -1
+
+        if ball.rect.x < 0:
+            finish = True 
+            window.blit(lose1, (180, 200))
+
+        if ball.rect.x > win_width:
+            finish = True 
+            window.blit(lose2, (180, 200))
+        
+        raket_1.reset()
+        raket_2.reset() 
+        ball.reset()
 
 
     display.update()
     clock.tick(FPS)
+
+
+
+
+    display.update()
+    clock.tick(FPS)
+
 
